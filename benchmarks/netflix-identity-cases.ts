72f1a316e66535f4d82d60801da99fcf9d089a3a
Jenifer Tabita Ciuciu-Kiss <72320455+kuefmz@users.noreply.github.com>
2026-08-31 16:29:46 +0200

    test: add 28-case Netflix identity benchmark

---
+++ benchmarks/netflix-identity-cases.ts
@@ -0,0 +1,179 @@
+export interface NetflixIdentityBenchmarkCase {
+  name: string;
+  documentary: string;
+  year: number;
+  netflixUrl: string;
+}
+
+export const netflixIdentityBenchmarkCases: NetflixIdentityBenchmarkCase[] = [
+  {
+    name: "Rachel Dolezal",
+    documentary: "The Rachel Divide",
+    year: 2018,
+    netflixUrl: "https://www.netflix.com/title/80149821",
+  },
+  {
+    name: "Bikram Choudhury",
+    documentary: "Bikram: Yogi, Guru, Predator",
+    year: 2019,
+    netflixUrl: "https://www.netflix.com/title/80221584",
+  },
+  {
+    name: "Ted Bundy",
+    documentary: "Conversations with a Killer: The Ted Bundy Tapes",
+    year: 2019,
+    netflixUrl: "https://www.netflix.com/title/80226612",
+  },
+  {
+    name: "Billy McFarland",
+    documentary: "FYRE: The Greatest Party That Never Happened",
+    year: 2019,
+    netflixUrl: "https://media.netflix.com/en/only-on-netflix/81035279",
+  },
+  {
+    name: "Aaron Hernandez",
+    documentary: "Killer Inside: The Mind of Aaron Hernandez",
+    year: 2020,
+    netflixUrl: "https://www.netflix.com/title/81062828",
+  },
+  {
+    name: "Elisa Lam",
+    documentary: "Crime Scene: The Vanishing at the Cecil Hotel",
+    year: 2021,
+    netflixUrl: "https://www.netflix.com/title/81183727",
+  },
+  {
+    name: "Rick Singer",
+    documentary: "Operation Varsity Blues: The College Admissions Scandal",
+    year: 2021,
+    netflixUrl: "https://media.netflix.com/en/only-on-netflix/81130691",
+  },
+  {
+    name: "Joe Exotic",
+    documentary: "Tiger King",
+    year: 2021,
+    netflixUrl: "https://www.netflix.com/title/81115994",
+  },
+  {
+    name: "Richard Ramirez",
+    documentary: "Night Stalker: The Hunt for a Serial Killer",
+    year: 2021,
+    netflixUrl: "https://www.netflix.com/title/81025701",
+  },
+  {
+    name: "Manti Te'o",
+    documentary: "Untold: The Girlfriend Who Didn't Exist",
+    year: 2022,
+    netflixUrl: "https://www.netflix.com/title/81580141",
+  },
+  {
+    name: "John McAfee",
+    documentary: "Running with the Devil: The Wild World of John McAfee",
+    year: 2022,
+    netflixUrl: "https://www.netflix.com/title/81327515",
+  },
+  {
+    name: "Carlos Ghosn",
+    documentary: "Fugitive: The Curious Case of Carlos Ghosn",
+    year: 2022,
+    netflixUrl: "https://www.netflix.com/title/81227167",
+  },
+  {
+    name: "Shimon Hayut",
+    documentary: "The Tinder Swindler",
+    year: 2022,
+    netflixUrl:
+      "https://www.netflix.com/tudum/articles/who-is-tinder-swindler-real-shimon-hayut",
+  },
+  {
+    name: "Jimmy Savile",
+    documentary: "Jimmy Savile: A British Horror Story",
+    year: 2022,
+    netflixUrl: "https://www.netflix.com/title/81520549",
+  },
+  {
+    name: "Warren Jeffs",
+    documentary: "Keep Sweet: Pray and Obey",
+    year: 2022,
+    netflixUrl: "https://www.netflix.com/title/81292539",
+  },
+  {
+    name: "Anthony Templet",
+    documentary: "I Just Killed My Dad",
+    year: 2022,
+    netflixUrl: "https://www.netflix.com/title/81290894",
+  },
+  {
+    name: "Lori Vallow",
+    documentary: "Sins of Our Mother",
+    year: 2022,
+    netflixUrl: "https://www.netflix.com/title/81300060",
+  },
+  {
+    name: "John Wayne Gacy",
+    documentary: "Conversations with a Killer: The John Wayne Gacy Tapes",
+    year: 2022,
+    netflixUrl: "https://www.netflix.com/title/81298614",
+  },
+  {
+    name: "Robert Hendy-Freegard",
+    documentary: "The Puppet Master: Hunting the Ultimate Conman",
+    year: 2022,
+    netflixUrl:
+      "https://www.netflix.com/tudum/articles/the-puppet-master-surprising-facts-from-the-con-mans-journey",
+  },
+  {
+    name: "Bernie Madoff",
+    documentary: "MADOFF: The Monster of Wall Street",
+    year: 2023,
+    netflixUrl: "https://www.netflix.com/title/81466159",
+  },
+  {
+    name: "Alex Murdaugh",
+    documentary: "Murdaugh Murders: A Southern Scandal",
+    year: 2023,
+    netflixUrl: "https://www.netflix.com/title/81519789",
+  },
+  {
+    name: "Lou Pearlman",
+    documentary: "Dirty Pop: The Boy Band Scam",
+    year: 2024,
+    netflixUrl: "https://www.netflix.com/title/81476403",
+  },
+  {
+    name: "Robert Shinn",
+    documentary: "Dancing for the Devil: The 7M TikTok Cult",
+    year: 2024,
+    netflixUrl: "https://www.netflix.com/title/81638162",
+  },
+  {
+    name: "Denise Huskins",
+    documentary: "American Nightmare",
+    year: 2024,
+    netflixUrl: "https://www.netflix.com/title/81620852",
+  },
+  {
+    name: "Aaron Quinn",
+    documentary: "American Nightmare",
+    year: 2024,
+    netflixUrl: "https://www.netflix.com/title/81620852",
+  },
+  {
+    name: "Lyle Menendez",
+    documentary: "The Menendez Brothers",
+    year: 2024,
+    netflixUrl: "https://www.netflix.com/title/81506509",
+  },
+  {
+    name: "Erik Menendez",
+    documentary: "The Menendez Brothers",
+    year: 2024,
+    netflixUrl: "https://www.netflix.com/title/81506509",
+  },
+  {
+    name: "Aileen Wuornos",
+    documentary: "Aileen: Queen of the Serial Killers",
+    year: 2025,
+    netflixUrl: "https://www.netflix.com/title/81673047",
+  },
+];
